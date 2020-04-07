49212469e163e32f55c7f7a0fc548420f1b0d53b
Lake Work <LakeWork@Lakes-MacBook-Pro.local>
2020-04-06 22:23:04 -0500

    continue to fill out game logic

--- src/Utils/logic.js
+++ src/Utils/logic.js
@@ -32,12 +32,46 @@ export const getBishopMoves = (board, curPos) => {
 
 //
 export const getKnightMoves = (board, curPos) => {
+    let offsets =  [[-2,1],
+                    [-2,-1],
+                    [2,1],
+                    [2,-1],
+                    [1,2],
+                    [-1,2],
+                    [1,-2],
+                    [-1,-2]]
+    let ret = []
 
+    offsets.forEach( e => {
+        let potentialPos = [curPos[0]+e[0],curPos[1]+e[1]]
+        if(isPosOnBoard(potentialPos)){
+            if(isPieceHere(potentialPos)){
+                if(!areFriends(board, curPos, potentialPos)){
+                    ret.push(potentialPos)
+                }
+            }
+            else
+            {
+                ret.push(potentialPos)
+            }
+        }
+    })
+
+    return ret
 }
 
 //
 export const getRookMoves = (board, curPos) => {
+    const offsets = [[1,0],[-1,0],[0,1],[0,-1]]
+    offsets.forEach( e => {
+        let potentialPos = [curPos[0]+e[0],curPos[1]+e[1]]
+        while(isPosOnBoard(potentialPos) && !isPieceHere(potentialPos)){
+            ret.push(potentialPos)
+            potentialPos = [potentialPos[0]+e[0], potentialPos[1]+e[1]]
+        }
 
+         
+    })
 }
 
 
@@ -45,12 +79,39 @@ export const isPieceInDanger = (board, curPos) => {
 
 }
 
-export const isPieceWhite = (board, curPos) => {
+export const isPieceWhite = (inPiece) => {
+    if(inPiece === null){
+        throw "isPieceWhite - inPiece is null"
+    }
 
+    switch(inPiece){
+        case PIECE.WHITE_KING:
+            return true;
+        case PIECE.WHITE_QUEEN:
+            return true;
+        case PIECE.WHITE_PAWN:
+            return true;
+        case PIECE.WHITE_BISHOP:
+            return true;
+        case PIECE.WHITE_KNIGHT:
+            return true;
+        case PIECE.WHITE_ROOK:
+            return true;
+        default:
+            return false;
+    }
 }
 
 export const areFriends = (board, aPos, bPos) => {
+    let aPiece = getPiece(board, aPos)
+    let bPiece = getPiece(board, bPos)
 
+    if(aPiece != null && bPiece != null){
+        if(isPieceWhite(aPiece) === isPieceWhite(bPiece)){
+            return true
+        }
+    }
+    return false
 }
 
 export const isPosOnBoard = (curPos) => {
@@ -61,4 +122,20 @@ export const isPosOnBoard = (curPos) => {
         return true
     }
     return false
+}
+
+export const getPiece = (board, pos) => {
+    let ret_piece = board[pos[0]][pos[1]]
+    return ret_piece
+}
+
+export const isPieceHere = (board, pos) => {
+    if(board[pos[0]][pos[1]] === null){
+        return false
+    }
+    return true
+}
+
+export const endangersKing = () => {
+
 }
